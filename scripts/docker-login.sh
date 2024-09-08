f193e3aec46d922d22f683ac7dd223c21021c7dd
AMEGBEDJI Romuald Fervic Précieux <126726559+Essogbe@users.noreply.github.com>
2024-09-08 18:50:39 +0000

    Update docker-login.sh

--- scripts/docker-login.sh
+++ scripts/docker-login.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Login to Scaleway container registry
-echo ${{ secrets.SCW_SECRET_KEY }}
-echo ${{ secrets.SCW_SECRET_KEY }} | docker login rg.fr-par.scw.cloud/atut-group-1 -u ${{ secrets.SCW_SECRET_KEY }} --password-stdin
+echo ${{SCW_SECRET_KEY }}
+echo ${{SCW_SECRET_KEY }} | docker login rg.fr-par.scw.cloud/atut-group-1 -u ${{ SCW_SECRET_KEY }} --password-stdin
