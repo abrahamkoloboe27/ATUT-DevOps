e44f05b4e0ce29d91aa31925a6778172476854a4
essogbe <amrromuald234@gmail.com>
2024-09-08 23:44:41 +0000

    Correct Workflow

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # Tag Docker image for backend with commit hash
 
-docker tag ${CONTAINER_REGISTRY_ENDPOINT}/backend:latest ${CONTAINER_REGISTRY_ENDPOINT}/backend:${COMMIT_HASH}
-# Tag Docker image for frontend with commit hash
-
-docker tag ${CONTAINER_REGISTRY_ENDPOINT}/frontend:latest ${CONTAINER_REGISTRY_ENDPOINT}/frontend:${COMMIT_HASH}
 for image in $IMAGES; do
     # Récupérer le nom de l'image
     image_name=$(basename "$image")
