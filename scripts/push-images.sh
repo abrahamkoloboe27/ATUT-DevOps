ab3704c3112fd886c973a113877f8f8d798d7210
AMEGBEDJI Romuald Fervic Précieux <126726559+Essogbe@users.noreply.github.com>
2024-09-08 19:04:51 +0000

    Update push-images.sh

--- scripts/push-images.sh
+++ scripts/push-images.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Push backend image to the registry
 
-docker push ${{ secrets.CONTAINER_REGISTRY_ENDPOINT }}/backend:${COMMIT_HASH}
-docker push ${{ secrets.CONTAINER_REGISTRY_ENDPOINT }}/backend:latest
-#!/bin/bash
+docker push ${CONTAINER_REGISTRY_ENDPOINT}/backend:${COMMIT_HASH}
+docker push ${CONTAINER_REGISTRY_ENDPOINT}/backend:latest
+
 # Push backend image to the registry
 
-docker push ${{ secrets.CONTAINER_REGISTRY_ENDPOINT }}/frontend:${COMMIT_HASH}
-docker push ${{ secrets.CONTAINER_REGISTRY_ENDPOINT }}/frontend:latest
+docker push ${CONTAINER_REGISTRY_ENDPOINT}/frontend:${COMMIT_HASH}
+docker push ${CONTAINER_REGISTRY_ENDPOINT}/frontend:latest
