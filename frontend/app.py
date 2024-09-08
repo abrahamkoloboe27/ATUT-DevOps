d1542f0b99db5ad2bdb4059b5f48e036a9eb7a50
Abraham KOLOBOE <abraham.koloboe@mehriso.com>
2024-09-08 13:28:20 +0100

    Refactor app.py to include name input field for user interaction

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title='ATUT-DevOps',
                    page_icon=':rocket:')
 
 st.title('Test ATUT-DevOps First Deployment')
-
+name_input = st.text_input('Enter your name')
 if st.button('Submit'):
     response = requests.get(f'http://api:8000/welcome/{name_input}')
     if response.status_code == 200:
