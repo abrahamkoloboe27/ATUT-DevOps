3f253b5ee0a17c0349116d470dc19cc166590a78
AMEGBEDJI Romuald Fervic Précieux <126726559+Essogbe@users.noreply.github.com>
2024-09-08 19:06:43 +0000

    Update tag.sh

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Tag Docker image for backend with commit hash
 
-docker tag ${{ secrets.CONTAINER_REGISTRY_ENDPOINT }}/backend:latest ${{ secrets.CONTAINER_REGISTRY_ENDPOINT }}/backend:${COMMIT_HASH}
+docker tag ${CONTAINER_REGISTRY_ENDPOINT}/backend:latest ${CONTAINER_REGISTRY_ENDPOINT}/backend:${COMMIT_HASH}
 # Tag Docker image for frontend with commit hash
 
-docker tag ${{ secrets.CONTAINER_REGISTRY_ENDPOINT }}/frontend:latest ${{ secrets.CONTAINER_REGISTRY_ENDPOINT }}/frontend:${COMMIT_HASH}
+docker tag ${CONTAINER_REGISTRY_ENDPOINT}/frontend:latest ${CONTAINER_REGISTRY_ENDPOINT}/frontend:${COMMIT_HASH}
