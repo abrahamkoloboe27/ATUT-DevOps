d714af812e95323f3f31a92598613328c0811337
Abraham KOLOBOE <abraham.koloboe@mehriso.com>
2024-09-08 11:24:13 +0100

    Refactor app.py to set page configuration and add name input

--- frontend/app.py
+++ frontend/app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import requests
 import json
+st.set_page_config(page_title='ATUT-DevOps', 
+                   page_icon=':rocket:')
 
 st.title('Test ATUT-DevOps')
 
-st.text_input('Name', 'Type your name here')
+
+name_input = st.text_input('Name', 'Type your name here')
 
 if st.button('Submit'):
-    response = requests.get(f'http://api:5000/welcome/{name_input}')
+    response = requests.get(f'http://api:8000/welcome/{name_input}')
     if response.status_code == 200:
         message = response.json().get('message', 'No message received')
         st.success(message)
